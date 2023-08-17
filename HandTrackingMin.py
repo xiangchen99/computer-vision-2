6be73702a50fe6ef4fc85549aa4018c5c449d131
xiang <xianc@seas.upenn.edu>
2023-08-17 17:26:35 -0400

    working on hand tracking module

---
+++ HandTrackingMin.py
@@ -0,0 +1,34 @@
+import cv2
+import mediapipe as mp
+import time
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
+mpDraw = mp.solutions.drawing_utils
+
+#show fps
+previousTime = 0
+currentTime = 0
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                if id == 0:
+                    cv2.circle(img, (cx, cy), 15, (255,255,0), cv2.FILLED)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    currentTime = time.time()
+    fps = 1/(currentTime - previousTime)
+    previousTime = currentTime
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,255,0), 3, 2)
+
+    cv2.imshow("image", img)
+    cv2.waitKey(1)
